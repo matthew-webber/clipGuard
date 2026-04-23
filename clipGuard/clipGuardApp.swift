0e103bb02894a27164229765ef67c3c2d3e9c730
Matt Webber <dev.matthew.lee.webber@gmail.com>
2026-04-23 14:46:20 -0400

    Initial Commit

---
+++ clipGuard/clipGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipGuardApp.swift
+//  clipGuard
+//
+//  Created by m on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct clipGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clipGuardTests/clipGuardTests.swift
@@ -0,0 +1,17 @@
+//
+//  clipGuardTests.swift
+//  clipGuardTests
+//
+//  Created by m on 4/23/26.
+//
+
+import Testing
+@testable import clipGuard
+
+struct clipGuardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
